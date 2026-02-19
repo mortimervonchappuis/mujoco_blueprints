27fd189c57b8582f914aa28a33aa1146ea73003a
mortimervonchappuis <mortimervonchappuis@protonmail.com>
2026-02-19 18:54:50 +0100

    added pypi update mirrortypes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as file:
 	long_description = file.read()
 
 setup(name='mujoco-blueprints', 
-      version='0.0.3', 
+      version='0.0.4', 
       packages=find_packages(), 
       author='Mortimer von Chappuis', 
       install_requires=['numpy>=2.0.0', 
@@ -14,4 +14,5 @@ setup(name='mujoco-blueprints',
       description='Blueprints is a Mujoco interface providing many graph manipulation operations and easy runtime data access.', 
       long_description=long_description, 
       long_description_content_type='text/markdown', 
+      url='https://mujoco-blueprints.readthedocs.io/en/latest/index.html', 
       download_url='https://github.com/mortimervonchappuis/mujoco_blueprints')
